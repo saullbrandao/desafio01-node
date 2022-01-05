6153a0f4f5f602e81eaa6fe46bd9b2fbfdae4a0e
Saull Brandão <saull@outlook.com>
2022-01-05 09:45:06 -0300

    feat: can get all todos from a user

--- src/index.js
+++ src/index.js
@@ -44,7 +44,10 @@ app.post("/users", (request, response) => {
   return response.status(201).json(user);
 });
 
-app.get("/todos", checksExistsUserAccount, (request, response) => {});
+app.get("/todos", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  return response.status(200).json(user.todos);
+});
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
   const { title, deadline } = request.body;
