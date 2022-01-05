bfe589d0f36e0a872e0b846698b65df45ffb1495
Saull Brandão <saull@outlook.com>
2022-01-05 09:54:20 -0300

    feat: can delete a todo

--- src/index.js
+++ src/index.js
@@ -82,11 +82,31 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+
+  const todo = request.user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
+  todo.done = !todo.done;
+
+  return response.status(200).json(todo);
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+
+  const todo = request.user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
+  request.user.todos.splice(todo, 1);
+
+  return response.status(204).send(todo);
 });
 
 module.exports = app;
